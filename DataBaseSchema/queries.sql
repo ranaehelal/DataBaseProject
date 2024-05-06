95c691aaf7ca12b6874be4f774c25472c5096fd1
ranaehelal <115326882+ranaehelal@users.noreply.github.com>
2024-05-06 16:57:01 +0300

    Create queries.sql

---
+++ DataBaseSchema/queries.sql
@@ -0,0 +1,56 @@
+--Retrieve All Books
+SELECT 
+    b.Book_ID,
+    b.Book_Title,
+    b.Book_Price,
+    b.Publication_Year,
+    b.ISBN,
+    a.[Name] AS Author_Name,
+    adm.Username AS Admin_Username
+FROM 
+    Book b
+LEFT JOIN 
+    Author a ON b.Author_ID = a.Author_ID
+LEFT JOIN 
+    [Admin] adm ON b.Admin_ID = adm.Admin_ID;
+
+--Retrieve Student Information
+SELECT 
+    s.Student_ID,
+    s.FName,
+    s.LName,
+    s.UserName,
+    s.Email,
+    s.[Address],
+    pn.PhoneNumber
+FROM 
+    Student s
+LEFT JOIN 
+    PhoneNumber pn ON s.Student_ID = pn.Student_ID;
+--Retrieve Book Orders
+SELECT 
+    o.Order_ID,
+    o.Borrow_Date,
+    o.Return_Date,
+    o.Due_Date,
+    s.FName AS Student_FName,
+    s.LName AS Student_LName,
+    b.Book_Title
+FROM 
+    [Order] o
+LEFT JOIN 
+    Student s ON o.Student_ID = s.Student_ID
+LEFT JOIN 
+    BookOrder bo ON o.Order_ID = bo.Order_ID
+LEFT JOIN 
+    Book b ON bo.Book_ID = b.Book_ID;
+ --Count of Books by Author
+SELECT 
+    a.[Name] AS Author_Name,
+    COUNT(b.Book_ID) AS Total_Books
+FROM 
+    Author a
+LEFT JOIN 
+    Book b ON a.Author_ID = b.Author_ID
+GROUP BY 
+    a.[Name];
