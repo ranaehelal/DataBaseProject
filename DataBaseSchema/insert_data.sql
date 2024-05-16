b190c618100cca794999e9cf21bc73689dbb485f
ranaehelal <115326882+ranaehelal@users.noreply.github.com>
2024-05-17 00:11:38 +0300

    Update insert_data.sql

--- DataBaseSchema/insert_data.sql
+++ DataBaseSchema/insert_data.sql
@@ -13,22 +13,18 @@ VALUES
 (2, 'George R.R. Martin', 'Author of A Song of Ice and Fire');
 
 -- Insert into Book Table
-INSERT INTO Book (Book_ID, Book_Title, Book_Price, Publication_Year, [Availability], No_copies, ISBN, Category, Publisher, Edition, Author_ID, Admin_ID)
+INSERT INTO Book (Book_ID, Book_Title, Book_Price, Publication_Year, [Availability], No_copies, ISBN, Category, Publisher, Edition, Author_ID)
 VALUES 
-(1, 'Harry Potter and the Sorcerers Stone', 19.99, 1997, 1, 10, '9780439708180', 'Fantasy', 'Scholastic', '1st', 1, 1),
-(2, 'A Game of Thrones', 24.99, 1996, 1, 15, '9780553103540', 'Fantasy', 'Bantam Books', '1st', 2, 2);
+(1, 'Harry Potter and the Sorcerers Stone', 19.99, 1997, 1, 10, '9780439708180', 'Fantasy', 'Scholastic', '1st', 1),
+(2, 'A Game of Thrones', 24.99, 1996, 1, 15, '9780553103540', 'Fantasy', 'Bantam Books', '1st', 2);
 
 -- Insert into Student Table
-INSERT INTO Student (Student_ID, FName, LName, UserName, [Password], Email, [Address])
+INSERT INTO Student (Student_ID, FName, LName, UserName, [Password], Email, [Address],PhoneNumber)
 VALUES 
-(1, 'Alice', 'Johnson', 'alicej', 'password789', 'alice.johnson@example.com', '789 Oak St'),
-(2, 'Bob', 'Williams', 'bobw', 'password012', 'bob.williams@example.com', '321 Pine St');
+(1, 'Alice', 'Johnson', 'alicej', 'password789', 'alice.johnson@example.com', '789 Oak St', '123-456-7890')),
+(2, 'Bob', 'Williams', 'bobw', 'password012', 'bob.williams@example.com', '321 Pine St', '098-765-4321');
 
--- Insert into PhoneNumber Table
-INSERT INTO PhoneNumber (Phone_ID, Student_ID, PhoneNumber)
-VALUES 
-(1, 1, '123-456-7890'),
-(2, 2, '098-765-4321');
+
 
 -- Insert into [Order] Table
 INSERT INTO [Order] (Order_ID, Borrow_Date, Return_Date, Due_Date, Student_ID)
